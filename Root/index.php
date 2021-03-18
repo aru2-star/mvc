869764238d5fc4246eb2c7684ca75725f97ef015
aru2-star <68215528+aru2-star@users.noreply.github.com>
2021-03-18 23:31:47 +0530

    Create index.php

---
+++ Root/index.php
@@ -0,0 +1,60 @@
+<?php
+
+
+class Mage
+{
+
+    public static function init()
+    {
+        self::loadFileByClassName('Controller_Core_Front');
+        Front::init();
+    }
+
+    public static function loadFileByClassName($className)
+    {
+        $className = str_replace('_', ' ', $className);
+        $className = ucwords($className);
+        $className = str_replace(' ', '/', $className);
+        $className = $className . '.php';
+        require_once('./' . $className);
+    }
+
+    public static function getController($className)
+    {
+        self::loadFileByClassName($className);
+        $className = str_replace('_', ' ', $className);
+        $className = ucwords($className);
+        $className = str_replace(' ', '_', $className);
+        return new $className;
+    }
+
+    public static function getBlock($className)
+    {
+        self::loadFileByClassName($className);
+        $className = str_replace('_', ' ', $className);
+        $className = ucwords($className);
+        $className = str_replace(' ', '_', $className);
+        return new $className;
+    }
+
+    public static function getModel($className)
+    {
+        self::loadFileByClassName($className);
+        $className = str_replace('_', ' ', $className);
+        $className = ucwords($className);
+        $className = str_replace(' ', '_', $className);
+        return new $className;
+    }
+
+    public static function getBaseDir($subPath = null) {
+        if ($subPath) {
+            # code...
+            return getcwd().DIRECTORY_SEPARATOR.$subPath;
+        }
+        return getcwd();
+    }
+}
+
+$mage = new Mage();
+Mage::init();
+?>
