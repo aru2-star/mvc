ccd5dd5d2b0f04c979e46843e3ae367b8c750172
aru2-star <68215528+aru2-star@users.noreply.github.com>
2021-03-18 23:37:29 +0530

    Add files via upload

---
+++ Root/Block/Admin/Attribute/Grid.php
@@ -0,0 +1,35 @@
+<?php
+
+Mage::loadFileByClassName('Block_Core_Template');
+
+class Block_Admin_Attribute_Grid extends Block_Core_Template {
+	protected $attributes = [];
+	protected $templateName = null;
+
+	public function __construct() {
+		$this->templateName  = './View/admin/attribute/grid.php';
+	}
+
+	public function setAttributes($attributes = null) {
+		if (!$attributes) {
+            $attribute = Mage::getModel('Model_Attributes');
+            $attributes = $attribute->fetchAll();
+
+            if ($attributes) {
+                $attributes = $attributes->getData();
+            }
+        }
+
+        $this->attributes = $attributes;
+        return $this;
+	}
+
+	public function getAttributes() {
+		if(!$this->attributes) {
+			$this->setAttributes();
+		}
+		return $this->attributes;
+	}
+}
+
+?>
