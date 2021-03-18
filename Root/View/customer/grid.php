da494463cbd8bc6680b0b79b6c4ad84928dc7420
aru2-star <68215528+aru2-star@users.noreply.github.com>
2021-03-19 00:29:37 +0530

    Add

--- Root/View/customer/grid.php
+++ Root/View/customer/grid.php
@@ -46,20 +46,3 @@ $customers = $this->getCustomers();
     </tbody>
 
 </table>
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Delete?</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                Are you sure you want to delete this particular contact?
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" id=deleteBtn data-bs-dismiss="modal" class="btn btn-danger">Delete</button>
-            </div>
-        </div>
-    </div>
-</div>
