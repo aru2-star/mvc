dddca7ff8c9b64820b30745f170e1aae554a80ba
aru2-star <68215528+aru2-star@users.noreply.github.com>
2021-03-18 23:35:51 +0530

    Create Edit.php

---
+++ Root/Block/Admin/Attribute/Edit.php
@@ -0,0 +1,32 @@
+<?php
+
+Mage::loadFileByClassName(
+'Block_Core_Template');
+class Block_Admin_Attribute_Edit extends Block_Core_Template {
+
+	protected $attribute = [];
+
+	public function __construct() {
+		 $this->templateName  = './View/admin/attribute/edit.php';
+	}
+
+	public function setAttribute($attribute = NULL) {
+		if(!$attribute){
+			$attribute = Mage::getModel('Model_Attribute');
+			if ($id = $this->getRequest()->getGet('attributeId')) {
+				$attribute = $attribute->load($id);
+			}
+		}
+		$this->attribute = $attribute;
+		return $this;
+	}
+
+	public function getAttribute() {
+		if (!$this->attribute) {
+			$this->setAttribute();
+		}
+		return $this->attribute;
+	}
+}
+
+?>
