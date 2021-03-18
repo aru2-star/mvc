d6b23dd7c224e33cd3c3c73ee7db39c5d8276eab
aru2-star <68215528+aru2-star@users.noreply.github.com>
2021-03-18 23:35:12 +0530

    Create Grid.php

---
+++ Root/Block/Admin/Attribute/Option/Grid.php
@@ -0,0 +1,34 @@
+<?php
+
+Mage::loadFileByClassName('Block_Core_Template');
+
+class Block_Admin_Attribute_Option_Grid extends Block_Core_Template {
+	protected $options = [];
+
+	public function __construct() {
+		$this->templateName  = './View/admin/attribute/option/grid.php';
+	}
+
+	public function setOptions($options = null) {
+		if (!$options) {
+            $option = Mage::getModel('Model_Attribute_Option');
+            $options = $option->fetchAll();
+            
+            if ($options) {
+                $options = $options->getData();
+            }
+        }
+
+        $this->options = $options;
+        return $this;
+	}
+
+	public function getOptions() {
+		if(!$this->options) {
+			$this->setOptions();
+		}
+		return $this->options;
+	}
+}
+
+?>
